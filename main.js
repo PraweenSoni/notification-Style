2ce14a98b2c95069c166b45dd709904a0dd78d53
pramrajkumar1234@gmail.com <pramrajkumar1234@gmail.com>
2023-02-20 19:52:55 +0530

    first

---
+++ main.js
@@ -0,0 +1,40 @@
+const notification = document.querySelector(".notification"),
+buttons = document.querySelectorAll(".buttons .btn");
+const toastDetails = {
+ timer: 5000,
+ success:{
+  icon: 'fa-circle-check',
+  Text: 'Success: This is Success.',
+ },
+ error:{
+  icon: 'fa-xmark',
+  Text: 'Error: This is Error',
+ },
+ warning:{
+  icon: 'fa-triangle-exclamation',
+  Text: 'Warning: This is Warning.',
+ },
+ info:{
+  icon: 'fa-circle-info',
+  Text: 'Info: This is Info.',
+ }
+}
+const removeToast = (toast) =>{
+ toast.classList.add("hide");
+ if (toast.timeoutId) clearTimeout(toast.timeoutId);
+ setTimeout(()=> toast.remove(), 500);
+}
+const createToast = (id) => {
+ const {icon, Text} = toastDetails[id];
+  const toast = document.createElement("li");
+  toast.className = `toast ${id}`;
+  toast.innerHTML = `<div class="column">
+    <i class="fa-solid ${icon}"></i>
+    <span>${Text}</span>
+   </div><i class = "fa-solid fa-xmark" onclick="removeToast(this.parentElement)"> </i>`;
+   notification.appendChild(toast);
+   toast.timeoutId = setTimeout(() => removeToast(toast), toastDetails.timer);
+}
+  buttons.forEach(btn => {
+   btn.addEventListener("click", () => createToast(btn.id));
+  });
